ed1296604c24174bb958a44f3640b549bff0e91a
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-05-28 16:09:05 +0200

    [add] inizio stampa in pagina

--- database/seeders/ProjectTypeTableSeeder.php
+++ database/seeders/ProjectTypeTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
+use App\Models\Type;
 
 class ProjectTypeTableSeeder extends Seeder
 {
@@ -12,6 +14,17 @@ class ProjectTypeTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        for ($i = 0; $i < 100; $i++) {
+
+            // estraggo un post random
+            $project = Project::inRandomOrder()->first();
+
+            // estraggo un id random dai type
+            $type_id = Type::inRandomOrder()->first()->id;
+
+            // aggiungo la relazione nella tabella pivot
+            $project->types()->attach($type_id);
+        }
     }
 }
